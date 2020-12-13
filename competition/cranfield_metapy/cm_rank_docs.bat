98577abbfc11a6f8c3f37110d4badcd788d4d2a2
cwebs <D7bmA9MwLb9wb6dcET>
2020-12-13 12:11:50 -0600

    0.5525767869790023 - bm25, hail mary 2

--- competition/cranfield_metapy/cm_rank_docs.bat
+++ competition/cranfield_metapy/cm_rank_docs.bat
@@ -3,7 +3,7 @@ call cm_set_vars.bat
 python %WORKDIR%\search_eval.py ^
     --run_type "test" ^
     --ranker "bm25" ^
-    --params "1.5;1.0;500" ^
+    --params "2.0;0.9;4450" ^
     --dat_keys "title" ^
     --doc_weights "1.0" ^
     --cranfield_dir "%WORKDIR%" ^
