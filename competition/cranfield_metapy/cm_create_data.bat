ba6145d5ebd0787bf0494dd0e9df201e902cb3ff
cwebs <D7bmA9MwLb9wb6dcET>
2020-12-13 12:07:33 -0600

    bm25, hail mary

--- competition/cranfield_metapy/cm_create_data.bat
+++ competition/cranfield_metapy/cm_create_data.bat
@@ -2,7 +2,7 @@ call cm_set_vars.bat
 
 python %WORKDIR%\create_cranfield.py ^
     --run_type "test" ^
-    --query_keys "query" ^
+    --query_keys "query;question;narrative" ^
     --doc_keys "title:abstract:intro" ^
     --cranfield_dir "%WORKDIR%" ^
     --input_dir "%JSON_DIR%"
--- competition/cranfield_metapy/cm_rank_docs.bat
+++ competition/cranfield_metapy/cm_rank_docs.bat
@@ -2,8 +2,8 @@ call cm_set_vars.bat
 
 python %WORKDIR%\search_eval.py ^
     --run_type "test" ^
-    --ranker "bm25+" ^
-    --params "1.0;0.38;1.0" ^
+    --ranker "bm25" ^
+    --params "1.5;1.0;500" ^
     --dat_keys "title" ^
     --doc_weights "1.0" ^
     --cranfield_dir "%WORKDIR%" ^
